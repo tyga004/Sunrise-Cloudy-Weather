b120f034c2b3881ada82e608e421d331fd7b1ed5
Momart25 <134068807+Momart25@users.noreply.github.com>
2023-10-16 04:43:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,16 +11,18 @@ def main():
     st.write("Instructor: Dr. Jonathan Taylar")
     st.title("Predicting Class Weather (Sunrise or Cloudy)")
     st.write(
-        "This program identifies submitted images according to their weather classification, whether they are Cloudy or Sunrise photos, using a pre-trained convolutional neural network model."
+        "This program identifies submitted images according to their weather classification,"
+        " whether they are Cloudy or Sunrise photos, using a pre-trained convolutional neural network model."
     )
 
-    @st.cache(allow_output_mutation=True)
+    @st.cache_resource
     def load_model():
         model = tf.keras.models.load_model("weights-improvement-10-0.99.hdf5")
         return model
 
     def import_and_predict(image_data, model):
-        image = cv2.resize(image_data, (128, 128))
+        image = np.asarray(image_data)
+        image = cv2.resize(image, (128, 128))
         image = image / 255.0
         image = np.expand_dims(image, axis=0)
         prediction = model.predict(image)
@@ -38,7 +40,10 @@ def main():
         st.text("Please upload an image file")
     else:
         image = Image.open(file)
+        image = image.resize((128, 128))
         image = np.asarray(image)
+        image = image / 255.0
+        image = np.expand_dims(image, axis=0)
         st.image(image, use_column_width=True)
         prediction = import_and_predict(image, model)
         class_index = np.argmax(prediction)
