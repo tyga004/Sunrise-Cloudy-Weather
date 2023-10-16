bd3c8a5f56703ee7edd6faced9b24b8e3f3aac8c
aaaaaadddsss <adriandayang@gmail.com>
2023-10-16 09:43:39 +0800

    Update

--- app.py
+++ app.py
@@ -4,15 +4,6 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 
-def login():
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-    if st.button("Login"):
-        if username == "user" and password == "user":
-            return True
-        else:
-            st.warning("Incorrect username or password")
-    return False
 
 def main():
     st.write("Group 4")
@@ -23,32 +14,38 @@ def main():
         "This program identifies submitted images whether they are Cloudy or Sunrise photos."
     )
 
-    if login():
-        st.text_input("Username", value="user", key="username_input")  # Hide username input
-        st.text_input("Password", type="password", value="user", key="password_input")  # Hide password input
+    @st.cache(allow_output_mutation=True)
+    def load_model():
+        model = tf.keras.models.load_model("weights-improvement-10-0.99.hdf5")
+        return model
 
-        uploaded = st.file_uploader(
-            "Choose a Cloudy or Sunrise picture from your computer",
-            type=["jpg", "png", "jpeg"],
-        )
+    def import_and_predict(image_data, model):
+        image = cv2.resize(image_data, (128, 128))
+        image = image / 255.0
+        image = np.expand_dims(image, axis=0)
+        prediction = model.predict(image)
+        return prediction
 
-        if uploaded is None:
-            st.text("Please upload an image file")
-        else:
-            image = Image.open(uploaded)
-            image = np.asarray(image)
-            st.image(image, use_column_width=True)
-            with st.spinner("Analyzing..."):
-                model = tf.keras.models.load_model("weights-improvement-10-0.99.hdf5")
-                class_names = ["CLOUDY", "SUNRISE"]
-                image_data = cv2.resize(image, (128, 128))
-                image_data = image_data / 255.0
-                image_data = np.expand_dims(image_data, axis=0)
-                prediction = model.predict(image_data)
-                class_index = np.argmax(prediction)
-                class_name = class_names[class_index]
-                string = "Prediction: " + class_name
-                st.success(string)
+    model = load_model()
+    class_names = ["CLOUDY", "SUNRISE"]
+
+    file = st.file_uploader(
+        "Choose a Cloudy or Sunrise picture from your computer",
+        type=["jpg", "png", "jpeg"],
+    )
+
+    if file is None:
+        st.text("Please upload an image file")
+    else:
+        image = Image.open(file)
+        image = np.asarray(image)
+        st.image(image, use_column_width=True)
+        prediction = import_and_predict(image, model)
+        class_index = np.argmax(prediction)
+        class_name = class_names[class_index]
+        string = "Prediction: " + class_name
+        st.success(string)
+
 
 if __name__ == "__main__":
     main()
