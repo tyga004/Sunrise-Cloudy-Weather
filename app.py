f00a4a1f2618f17d14e3831829b4a87c61549aaf
aaaaaadddsss <adriandayang@gmail.com>
2023-10-19 22:09:05 +0800

    Update

--- app.py
+++ app.py
@@ -16,43 +16,39 @@ def login():
             st.error("Incorrect username or password")
     return False
 
-@st.cache(allow_output_mutation=True)
-def load_model():
-    model = tf.keras.models.load_model("weights-improvement-10-0.99.hdf5")
-    return model
-
-def run_prediction(model, logged_in):
-    if not logged_in:
-        st.warning("You need to log in to access the Prediction page.")
-        return
-
+def run_prediction():
     st.title("Prediction")
+    @st.cache(allow_output_mutation=True)
+    def load_model():
+        model = tf.keras.models.load_model("weights-improvement-10-0.99.hdf5")
+        return model
+
+    def import_and_predict(image_data, model):
+        image = cv2.resize(image_data, (128, 128))
+        image = image / 255.0
+        image = np.expand_dims(image, axis=0)
+        prediction = model.predict(image)
+        return prediction
+
+    model = load_model()
     class_names = ["CLOUDY", "SUNRISE"]
 
-    uploaded_image = st.file_uploader(
+    file = st.file_uploader(
         "Choose a Cloudy or Sunrise picture from your computer",
         type=["jpg", "png", "jpeg"],
     )
 
-    if uploaded_image is not None:
-        image = Image.open(uploaded_image)
+    if file is None:
+        st.text("Please upload an image file")
+    else:
+        image = Image.open(file)
+        image = np.asarray(image)
         st.image(image, use_column_width=True)
-        image_data = np.asarray(image)
-        st.write(f"Image shape: {image_data.shape}")
-        
-        prediction = import_and_predict(image_data, model)
-        st.write(f"Prediction shape: {prediction.shape}")
-
+        prediction = import_and_predict(image, model)
         class_index = np.argmax(prediction)
         class_name = class_names[class_index]
-        st.success("Prediction: " + class_name)
-
-def import_and_predict(image_data, model):
-    image = cv2.resize(image_data, (128, 128))
-    image = image / 255.0
-    image = np.expand_dims(image, axis=0)
-    prediction = model.predict(image)
-    return prediction
+        string = "Prediction: " + class_name
+        st.success(string)
 
 def check_login(username, password):
     # Replace with your authentication logic
@@ -64,18 +60,14 @@ def main():
     st.title("Group 4")
     st.title("Section: CPE 028 - CPE41S5")
     st.title("Instructor: Dr. Jonathan Taylar")
-
-    logged_in = False
-    model = load_model()  # Load the model outside of the run_prediction function
-
+    
     page = st.selectbox("Select Page", ["Login", "Prediction"])
-
+    
     if page == "Login":
-        logged_in = login()
-        if logged_in:
+        if login():
             st.subheader("Welcome to the Prediction Page")
-    elif page == "Prediction" and logged_in:
-        run_prediction(model, logged_in)
+    elif page == "Prediction":
+        run_prediction()
 
 if __name__ == "__main__":
     main()
