c509b60fc6617228b1ec163bf7bb026f57261f11
aaaaaadddsss <adriandayang@gmail.com>
2023-10-19 22:02:54 +0800

    Update

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def main():
         logged_in = login()
         if logged_in:
             st.subheader("Welcome to the Prediction Page")
-
+            run_prediction()
     elif page == "Prediction" and logged_in:
         run_prediction()
 
